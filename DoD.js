09e4eeb50b00148c69ba58874c717ffcb62afa1e
Gustavo Semeao Maltoni <gmaltoni@ciandt.com>
2018-03-21 10:35:26 -0300

    Updating DoD format

--- DoD.js
+++ DoD.js
@@ -1,93 +1,93 @@
 dodBacklog = [
-  "1. Prioritization has been completed."
+  "Prioritization has been completed."
 ];
 
 dodBusinessGrooming = [
-  "1. Story slicing.",
-  "2. CPs calculated.",
-  "3. Story details reach and requirements.",
-  "4. Story Open, Tech Grooming, Business Grooming were created.",
-  "5. Acceptance criteria created.",
-  "6. Log hours on sub-ticket Requirement.",
-  "7. All Blocks have been resolved."
+  "Story slicing.",
+  "CPs calculated.",
+  "Story details reach and requirements.",
+  "Story Open, Tech Grooming, Business Grooming were created.",
+  "Acceptance criteria created.",
+  "Log hours on sub-ticket Requirement.",
+  "All Blocks have been resolved."
 ];
 
 dodTechnicalGrooming = [
-  "1. Solution has been reviewed with the team.",
-  "2. Impact analysis for this solution has been done.",
-  "3. POCs were created (if necessary).",
-  "4. Resolution was documented on JIRA.",
-  "5. Acceptance Criteria updated.",
-  "6. Validate with Data Business Analytics if it requires Google Tag Manager updates.",
-  "7. Log hours on sub-ticket Tech Solution.",
-  "8. SCM and CI.",
-  "9. Story was split into sub-tasks."
+  "Solution has been reviewed with the team.",
+  "Impact analysis for this solution has been done.",
+  "POCs were created (if necessary).",
+  "Resolution was documented on JIRA.",
+  "Acceptance Criteria updated.",
+  "Validate with Data Business Analytics if it requires Google Tag Manager updates.",
+  "Log hours on sub-ticket Tech Solution.",
+  "SCM and CI.",
+  "Story was split into sub-tasks."
 ];
 
 dodDevelopment = [
-  "1. Story Open rite was done.",
-  "2. Story was split into sub-tasks.",
-  "3. Check if the Original Estimate Hours is equal zero.",
-  "4. Issues on Sonar, Lint and PHPCS were fixed.",
-  "5. Verify and fix warnings/errors on Drupal watchdog and/or PHP logs.",
-  "6. Verify project standards.",
-  "7. Validate if cache solution defined for the task is working.",
-  "8. Updated task status, commented relevant informations.",
-  "9. Validate if the development reaches the Acceptance Criteria.",
-  "10. [Unit Tests] Validate if the development reaches the Test Cases.",
-  "11. Root cause analysis for bugs.",
-  "12. Logged hours on task (Sub-Imp).",
-  "13. All items on Acceptance Criteria are OK.",
-  "14. The code was committed and merged/cherry-picked.",
-  "15. Ran build and validated code on Acquia DEV or DEV2 environment.",
-  "16. It was tested on devices and supported browsers.",
-  "17. Console log was checked (there’s no error/warning/messages).",
-  "18. Peer review was done (if applicable).",
-  "19. Behat peer review was done (if applicable).",
-  "20. Check if the Estimate Hours is equal to zero in the Story."
+  "Story Open rite was done.",
+  "Story was split into sub-tasks.",
+  "Check if the Original Estimate Hours is equal zero.",
+  "Issues on Sonar, Lint and PHPCS were fixed.",
+  "Verify and fix warnings/errors on Drupal watchdog and/or PHP logs.",
+  "Verify project standards.",
+  "Validate if cache solution defined for the task is working.",
+  "Updated task status, commented relevant informations.",
+  "Validate if the development reaches the Acceptance Criteria.",
+  "[Unit Tests] Validate if the development reaches the Test Cases.",
+  "Root cause analysis for bugs.",
+  "Logged hours on task (Sub-Imp).",
+  "All items on Acceptance Criteria are OK.",
+  "The code was committed and merged/cherry-picked.",
+  "Ran build and validated code on Acquia DEV or DEV2 environment.",
+  "It was tested on devices and supported browsers.",
+  "Console log was checked (there’s no error/warning/messages).",
+  "Peer review was done (if applicable).",
+  "Behat peer review was done (if applicable).",
+  "Check if the Estimate Hours is equal to zero in the Story."
 ];  
 
 dodCodeReview = [
-  "1. Verify if SCM, code and database standards were followed.",
-  "2. Hours logged in Code review Sub-imp.",
-  "3. Bugs tickets were closed (if applicable).",
-  "4. Check if the Estimate Hours is equal to zero in the Story.",
-  "5. Story status was updated."
+  "Verify if SCM, code and database standards were followed.",
+  "Hours logged in Code review Sub-imp.",
+  "Bugs tickets were closed (if applicable).",
+  "Check if the Estimate Hours is equal to zero in the Story.",
+  "Story status was updated."
 ];
 
 dodQA = [
-  "1. Test cases were closed on Jira.",
-  "2. Tests were executed and hours logged on sub-ticket.",
-  "3. Bugs tickets were closed (if applicable).",
-  "4. Check if the Remaining Hours is equal to zero in the Story.",
-  "5. Story status was updated."
+  "Test cases were closed on Jira.",
+  "Tests were executed and hours logged on sub-ticket.",
+  "Bugs tickets were closed (if applicable).",
+  "Check if the Remaining Hours is equal to zero in the Story.",
+  "Story status was updated."
 ];
 
 dodBugFixing = [
-  "1. Bug’s Root Cause was filled.",
-  "2. Bug’s Root Cause notes was filled.",
-  "3. Resolution field of bug was filled.",
-  "4. Logged hours on task."
+  "Bug’s Root Cause was filled.",
+  "Bug’s Root Cause notes was filled.",
+  "Resolution field of bug was filled.",
+  "Logged hours on task."
 ];
 
 dodInMTS = [
-  "1. All requirements were executed and delivered to customer.",
-  "2. All changes in db were made.",
-  "3. All code were committed to STG environment.",
-  "4. Were realized a SMOKE or REGRESSION Test (whatever is needed).",
-  "5. The customer was notified with all needed documents SDLC to UAT."
+  "All requirements were executed and delivered to customer.",
+  "All changes in db were made.",
+  "All code were committed to STG environment.",
+  "Were realized a SMOKE or REGRESSION Test (whatever is needed).",
+  "The customer was notified with all needed documents SDLC to UAT."
 ];
 
 dodInUAT = [
-  "1. The tests were done by customer.",
-  "2. Bugs were opened? If YES, Development PHASE should be UAT.",
-  "3. All opened bugs were closed.",
-  "4. The SDLC files with testing checklist = PASSED were sent."
+  "The tests were done by customer.",
+  "Bugs were opened? If YES, Development PHASE should be UAT.",
+  "All opened bugs were closed.",
+  "The SDLC files with testing checklist = PASSED were sent."
 ];
 
 dodInMTP = [
-  "1. All requirements were executed and delivered to customer.",
-  "2. All changes in db were made.",
-  "3. All code were committed to PROD environment.",
-  "4. Were realized a SMOKE or REGRESSION Test."
+  "All requirements were executed and delivered to customer.",
+  "All changes in db were made.",
+  "All code were committed to PROD environment.",
+  "Were realized a SMOKE or REGRESSION Test."
 ];
